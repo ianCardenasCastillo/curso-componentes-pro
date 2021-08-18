bd3e63e3fafa295ad9ac44a095d620fa6f3ac8b8
Ian Cárdenas C <icardenac@bancoripley.com>
2021-08-18 14:07:00 -0400

    Se actualiza el codigo fuente: - Se actualiza el codigo fuente para el desarrollo de la seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { CountdownComponent } from './countdown/countdown.component';
+import { AlertViewComponent } from './alert-view/alert-view.component';
+import { DisplayComponent } from './display/display.component';
+import { SimpleAlertViewComponent } from './simple-alert-view/simple-alert-view.component';
+import { TabComponent } from './tab/tab.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TimerComponent } from './timer/timer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProgressBarComponent,
-    CountdownComponent
+    CountdownComponent,
+    AlertViewComponent,
+    DisplayComponent,
+    SimpleAlertViewComponent,
+    TabComponent,
+    TabsComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
